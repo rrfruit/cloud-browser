03c01abaf76e8f1606df5e5e2feabfe80f1d53bb
renkun <5196787+kundage@user.noreply.gitee.com>
2026-03-28 07:44:24 +0800

    111

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -21,7 +21,9 @@ fi
 echo "==> 启动容器: ${CONTAINER_NAME}"
 docker run \
   --name "${CONTAINER_NAME}" \
-  -p 15732:15732 \
+  --network="host" \
+  -e HTTP_PROXY="http://127.0.0.1:15732" \
+  -e HTTPS_PROXY="http://127.0.0.1:15732" \
   -p 9222:9222 \
   -p 9221:9221 \
   -p 9223-9323:9223-9323 \
