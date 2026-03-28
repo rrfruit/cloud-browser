f46b77f955d0d34d63758ebc114572fdad62a0f6
renkun <5196787+kundage@user.noreply.gitee.com>
2026-03-28 12:43:03 +0800

    1111

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,20 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import healthRouter from "./routes/health.js";
 import browserRouter from "./routes/browser.js";
+import { createSession } from "./browser.js";
 
+const sessionId = "session123123123123";
+const args: string[] = [];
 
 const app = new Hono().use(logger());
 
 app.route("/health", healthRouter);
 app.route("/browser", browserRouter);
+
+setTimeout(async () => {
+  const result = await createSession(sessionId, args);
+  console.log(result);
+}, 3000);
+
 
 export default app;
